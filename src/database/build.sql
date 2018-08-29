d62dd3d2d4bdb6ad185676e8a62d81184fae4916
ali <ali.7@outlook.com>
2018-08-29 14:12:53 +0300

    modify-sql

--- src/database/build.sql
+++ src/database/build.sql
@@ -15,20 +15,20 @@ INSERT INTO users (name,email,password) VALUES
  CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     post_body TEXT,
-    poster_id INTEGER REFERENCES users(user_id)
+    user_id INTEGER REFERENCES users(user_id)
 );
 
-INSERT INTO posts (post_body, poster_id) VALUES
+INSERT INTO posts (post_body, user_id) VALUES
 ('helllllllllllllo',1);
 
 CREATE TABLE comments (
   comment_id SERIAL PRIMARY KEY,
   comment_body TEXT NOT NULL,
-  commenter_id INTEGER REFERENCES users(user_id),
+  user_id INTEGER REFERENCES users(user_id),
   post_id INTEGER REFERENCES posts(post_id)
 );
 
-INSERT INTO comments(comment_body,commenter_id, post_id) VALUES
+INSERT INTO comments(comment_body,user_id, post_id) VALUES
 ('this is comment ',1,1);
 
 COMMIT;
