4ce73a0615df58c9415f83d3e4b9d077fa37655e
salwaMugh <Sam8O.o@hotmail.com>
2018-08-29 16:01:29 +0300

    adding addData

--- src/database/queries/addData.js
+++ src/database/queries/addData.js
@@ -8,3 +8,21 @@ const addUser = (user) => {
   } = user;
   return db.query('INSERT INTO users(name, email, password) VALUES ($1,$2,$3) RETURNING user_id', [name, email, password]);
 };
+
+const addPost = (post) => {
+  const {
+    body,
+    userid,
+  } = post;
+  return db.query('INSERT INTO posts (post_body,user_id) VALUES ($1,$2) RETURNING post_id', [body, userid]);
+};
+
+const addComment = (comment) => {
+  const {
+    body,
+    userid,
+    postid,
+  } = comment;
+  return db.query('INSERT INTO comments (comment_body,user_id ,post_id) VALUES ($1,$2,$3) RETURNING comment_id', [body, userid, postid]);
+};
+module.exports = { addUser, addPost, addComment };
