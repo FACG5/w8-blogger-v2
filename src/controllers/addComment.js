dd5718a9b70e767492695ddc55cffcd4a468dd95
ali <ali.7@outlook.com>
2018-08-30 09:53:23 +0300

    add-comment-route

---
+++ src/controllers/addComment.js
@@ -0,0 +1,16 @@
+const { post } = require('./../database/queries/addData');
+
+exports.postComment = ({ body }, res, next) => {
+  //user id from cokce
+  console.log(body);
+  const obj = { 
+    body,
+    userid,
+    postid,
+  }
+  // post.addComment(body,user,post).then(()=>{
+
+  //   res.redirect('/');
+  // }).catch(err => console.log(err));
+ 
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,7 @@ const home = require('./home');
 const error = require('./error');
 const user = require('./servePage');
 const auth = require('./authentication');
+const comment = require('./addComment');
 
 const router = express.Router();
 
@@ -11,8 +12,11 @@ const router = express.Router();
 router.get('/', home.get);
 router.get('/login', user.login);
 router.get('/signup', user.signup);
+
 router.post('/login', auth.login);
 router.post('/signup', auth.signup);
+
+router.post('/comment/create', comment.postComment);
 
 router.use(error.pageNotFound);
 router.use(error.serverError);
